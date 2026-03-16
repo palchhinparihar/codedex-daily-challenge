fa071781b38c7d35b5a88cb55a8833ee222a2da8
palchhin <palchhinparihar@gmail.com>
2026-03-16 20:34:46 +0530

    feat: add day 14 submission

---
+++ code/day14.py
@@ -0,0 +1,19 @@
+def decode_message(message, shift):
+  # alphabets list to map the characters
+  alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    
+  original_text = ''
+    
+  for char in message:
+    # storing the space character as it is
+    if char == ' ':
+      original_text += ' '
+    else:
+      # finding the index of the original character and adding it to the result
+      index = (ord(char) - ord('a') - shift) % 26
+      original_text += alphabets[index]
+        
+  return original_text
+    
+
+print(decode_message('aj xlmwtev xmnq lcsr ohhwx', 4))
