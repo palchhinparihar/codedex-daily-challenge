db30b890f6acd7962073b6b82135f4e82225f4fa
palchhin <palchhinparihar@gmail.com>
2026-03-17 19:27:00 +0530

    feat: add day 15 submission

---
+++ code/day15.py
@@ -0,0 +1,39 @@
+def oscar_pool(predictions):
+  # actual winners list
+  actual_winners = ['One Battle After Another', 'Michael B. Jordan', 'Jessie Buckley', 'Paul Thomas Anderson']
+
+  highest_pred = {}
+
+  # check from predictions for particular name - if they predictions match with actual winners
+  for name, picture, actor, actress, director in predictions:
+    highest_pred[name] = 0
+
+    if picture == actual_winners[0]:
+      highest_pred[name] += 1
+    if actor == actual_winners[1]:
+      highest_pred[name] += 1
+    if actress == actual_winners[2]:
+      highest_pred[name] += 1
+    if director == actual_winners[3]:
+      highest_pred[name] += 1
+  
+  ans = ''
+  highest_prediction = 0
+
+  # find the person with highest score -  if tie simple return "Tie"
+  for name, count in highest_pred.items():
+    if count > highest_prediction:
+      highest_prediction = count
+      ans = name
+    elif highest_prediction == count:
+      ans = 'Tie'
+    
+  return ans
+
+
+predictions = [
+  ["Rotten Tomatoes", "The Secret Agent", "Wagner Moura", "Renate Reinsve", "Kleber Mendonça Filho"],
+  ["IMDb", "One Battle After Another", "Timothée Chalamet", "Jessie Buckley", "Chloé Zhao"]
+]
+
+print(oscar_pool(predictions))
